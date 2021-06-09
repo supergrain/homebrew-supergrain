765ea85541cff2b81ca9cd040582c847aaeac1e7
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2021-06-09 11:09:19 -0700

    Update supergrain-cli.rb

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -6,7 +6,7 @@ class SupergrainCli < Formula
   url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.38.tar.gz"
   sha256 "59a596063fd8daad507d0f16b0c2fb49e170997cb2cd09f766dc00ddf830eaef"
   license "ISC"
-  release 3
+  release 4
 
   option "without-dbt", "Don't install dbt"
 
