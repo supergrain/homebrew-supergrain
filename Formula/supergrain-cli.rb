7ee90e4ff88bc2e0c936263dfa4a3abb4c3c1e45
Lucas Marshall <lucas@supergrain.com>
2021-06-09 11:13:34 -0700

    Fix formulas

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -1,17 +1,16 @@
 class SupergrainCli < Formula
-  include Language::Python::Virtualenv
 
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
   url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.38.tar.gz"
   sha256 "59a596063fd8daad507d0f16b0c2fb49e170997cb2cd09f766dc00ddf830eaef"
   license "ISC"
-  release 4
+  revision 4
 
   option "without-dbt", "Don't install dbt"
 
   depends_on "fishtown-analytics/dbt/dbt" => ["0.19.1", :recommended]
-  
+
   def install
       bin.install "sg"
   end
