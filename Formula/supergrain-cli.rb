27a7ea118446e7646f54f338a8bc668337ea2843
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2021-06-09 11:07:37 -0700

    Update supergrain-cli.rb

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -6,7 +6,7 @@ class SupergrainCli < Formula
   url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.38.tar.gz"
   sha256 "59a596063fd8daad507d0f16b0c2fb49e170997cb2cd09f766dc00ddf830eaef"
   license "ISC"
-  release 1
+  release 2
 
   option "without-snowflake-connector", "Don't install snowflake-connector"
   option "without-dbt", "Don't install dbt"
