356339a2af1e6b0d3a8110f152bf3a040b7a487e
lucasmarshall <lucasmarshall@users.noreply.github.com>
2021-06-24 17:46:23 +0000

    Sync versioned formula

---
+++ Formula/supergrain-cli@0.10.51.rb
@@ -0,0 +1,16 @@
+class SupergrainCliAT01051 < Formula
+
+  desc "Supergrain CLI"
+  homepage "http://supergrain.com"
+  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.51.tar.gz"
+  sha256 "a62f771102ec3f961016c86e74794370bc7908ccf640849a3ee72158555ccf1c"
+  license "ISC"
+
+  option "without-dbt", "Don't install dbt"
+
+  depends_on "fishtown-analytics/dbt/dbt" => ["0.19.1", :recommended]
+
+  def install
+      bin.install "sg"
+  end
+end
