1f5b3a5b9822ee24d23b09ec99c79a240d594e68
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2021-07-01 20:00:59 -0700

    Remove dbt dependency

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -5,10 +5,7 @@ class SupergrainCli < Formula
   url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.54.tar.gz"
   sha256 "fc2cffb01acd66885f8148b8a11d89e4dd53cee791db54ec4bceba1227cf69f0"
   license "ISC"
-
-  option "without-dbt", "Don't install dbt"
-
-  depends_on "fishtown-analytics/dbt/dbt" => ["0.19.1", :recommended]
+  revision 1
 
   def install
       bin.install "sg"
