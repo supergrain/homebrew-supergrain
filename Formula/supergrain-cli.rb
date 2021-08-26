1e0fb57cd8205b349609793d6d49cdab94103c17
Lucas Marshall <lucas@supergrain.com>
2021-08-26 11:35:08 -0700

    Revert "supergrain-cli development-notifs"
    This reverts commit 4c12993ef3e8958e4b8e853f0fbf0bb78d43a8f0.

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -2,8 +2,8 @@ class SupergrainCli < Formula
 
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
-  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.11.0.tar.gz"
-  sha256 "e17a930ce66c87604ea8dbe87dc96b02610ff1fac6f23694ec0e5da830b2e23e"
+  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.69.tar.gz"
+  sha256 "8a2a61d28dec104568b2274a37d21a8e2ec0ff13a699eed7c86007dd6ae3b7cb"
   license "ISC"
 
   def install
