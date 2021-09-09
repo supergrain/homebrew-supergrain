7cc8d5d42d410493d5d634ca1e9334ac6595b725
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2021-09-08 17:19:53 -0700

    supergrain-cli development-notifs
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -2,8 +2,8 @@ class SupergrainCli < Formula
 
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
-  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.76.tar.gz"
-  sha256 "fe718c8bda4311f95ef7678b12415968a1b7eaf1229a84920d797385af7c371c"
+  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.77.tar.gz"
+  sha256 "edaf5d8b0b7b77e927f5f2cadf7d8f34e890aba9b268108b235b70b15ffe4d3a"
   license "ISC"
 
   def install
