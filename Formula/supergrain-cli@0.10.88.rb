9795b8467d9cc7f5235b24783317efa5ac6ddec0
lucasmarshall <lucasmarshall@users.noreply.github.com>
2021-10-08 19:16:05 +0000

    Sync versioned formula

---
+++ Formula/supergrain-cli@0.10.88.rb
@@ -0,0 +1,12 @@
+class SupergrainCliAT01088 < Formula
+
+  desc "Supergrain CLI"
+  homepage "http://supergrain.com"
+  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.88.tar.gz"
+  sha256 "eaf50c375b8faf7d9ff44ca321110c7b1cd04a4ea6e5c4de0934437d363363d1"
+  license "ISC"
+
+  def install
+      bin.install "sg"
+  end
+end
