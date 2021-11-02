9accc23aaa371fad6185f1ee551add0f182d03e3
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2021-11-02 16:09:31 -0700

    Update formulas with cdn url (#16)

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -2,7 +2,7 @@ class SupergrainCli < Formula
 
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
-  url "https://propel-cli.s3.us-east-2.amazonaws.com/builds/sg-cli-v0.10.92.tar.gz"
+  url "https://cdn.supergrain.com/builds/sg-cli-v0.10.92.tar.gz"
   sha256 "c2800df1cabaae87fd781cb51e0c5278c588bfae450869f8e3e9c48800b9311e"
   license "ISC"
 
