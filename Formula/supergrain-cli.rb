eca39b2b43278c8aebebe3a8fc001679d8fc9551
Albert <albert@supergrain.com>
2021-11-05 22:55:54 -0700

    fix version

--- Formula/supergrain-cli.rb
+++ Formula/supergrain-cli.rb
@@ -2,7 +2,7 @@ class SupergrainCli < Formula
 
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
-  url "https://cdn.supergrain.com/builds/sg-cli-vv0.0.10.tar.gz"
+  url "https://cdn.supergrain.com/builds/sg-cli-v0.0.10.tar.gz"
   sha256 "5df946a9a33ff43c5e47949035f77d51fbad1546500f054aba41965485991ebc"
   license "ISC"
 
