8fb932ec0b666a2512e76686da4f600e50dcb42e
Lucas Marshall <lucas@supergrain.com>
2021-11-29 21:42:29 -0800

    Fix sha

--- Formula/supergrain-cli@0.1.3.rb
+++ Formula/supergrain-cli@0.1.3.rb
@@ -3,9 +3,9 @@ class SupergrainCliAT013 < Formula
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
   url "https://cdn.supergrain.com/builds/sg-cli-v0.1.3.tar.gz"
-  sha256 "0387bf552bd0972b0295077721a39f465fdfa1aac3e1e688178566c39ec50055"
+  sha256 "4adcc6f1cd428ab8c4109a9ab63508c6fb13fd9298164a1d1de4dae9167da7db"
   license "ISC"
-  revision 1
+  revision 2
 
   def install
       bin.install "sg"
