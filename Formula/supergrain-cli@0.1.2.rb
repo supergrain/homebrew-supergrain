1f24d7fda6041b1384d79e87c223feee5dd3eebb
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2021-11-29 17:00:36 -0800

    Update supergrain-cli@0.1.2.rb

--- Formula/supergrain-cli@0.1.2.rb
+++ Formula/supergrain-cli@0.1.2.rb
@@ -3,9 +3,9 @@ class SupergrainCliAT012 < Formula
   desc "Supergrain CLI"
   homepage "http://supergrain.com"
   url "https://cdn.supergrain.com/builds/sg-cli-v0.1.2.tar.gz"
-  sha256 "9abff0e1a2a731a2db74833244208bdaf1fa9f1b236b6e95950146c1c93b8a4f"
+  sha256 "9a007508574586249137a7dca4b1fda1e43427ba0e368a40ec589dc795a20221"
   license "ISC"
-  revision 3
+  revision 4
 
   def install
       bin.install "sg"
